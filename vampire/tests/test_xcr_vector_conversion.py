63d510f99c03a9c0ab8d49311c6c838df5fba361
kmayerb <kmayerb@gmail.com>
2020-03-11 19:04:17 -0700

    Add chain conditional tests

--- vampire/tests/test_xcr_vector_conversion.py
+++ vampire/tests/test_xcr_vector_conversion.py
@@ -1,8 +1,109 @@
 import numpy as np
 import pytest
-
+import json
 import vampire.common as common
 import vampire.xcr_vector_conversion as conversion
+import os
+
+
+def of_json_file(fname):
+    with open(fname, 'r') as fp:
+        return json.load(fp)
+
+# THIS BLOCK WAS ADDED TO PROVIDE CONTROL OF CHAIN USAGE, 
+# RIGHT NOW THIS IS CONTROLED WITH A SEPARATE FILE.
+# IT IS MY INTENT THAT CHAIN COULD BE SPECIFIED IN THE model_spec.json file
+path_to_test= os.path.dirname(os.path.realpath(__file__))
+path_to_vampire = os.path.dirname(path_to_test)
+fname_spec = os.path.join(  path_to_vampire, 
+                            'multichain_support',
+                            'multichain_spec.json')
+if os.path.isfile(fname_spec):
+    CHAIN = of_json_file(fname = fname_spec)['chain']
+else:
+    CHAIN = "beta"
+
+
+@pytest.mark.skipif(CHAIN != "beta",
+                    reason="This test requires beta chain mode")
+def test_gene_conversion():
+    for gene in conversion.TCRB_V_GENE_LIST:
+        assert conversion.onehot_to_vgene(conversion.vgene_to_onehot(gene)) == gene
+    for gene in conversion.TCRB_J_GENE_LIST:
+        assert conversion.onehot_to_jgene(conversion.jgene_to_onehot(gene)) == gene
+
+@pytest.mark.skipif(CHAIN != "delta",
+                    reason="This test requires delta chain mode")
+def test_gene_conversion_delta():
+    for gene in conversion.TCRD_V_GENE_LIST:
+        assert conversion.onehot_to_vgene(conversion.vgene_to_onehot(gene)) == gene
+    for gene in conversion.TCRD_J_GENE_LIST:
+        assert conversion.onehot_to_jgene(conversion.jgene_to_onehot(gene)) == gene
+
+@pytest.mark.skipif(CHAIN != "gamma",
+                    reason="This test requires gamma chain mode")
+def test_gene_conversion_gamma():
+    for gene in conversion.TCRG_V_GENE_LIST:
+        assert conversion.onehot_to_vgene(conversion.vgene_to_onehot(gene)) == gene
+    for gene in conversion.TCRG_J_GENE_LIST:
+        assert conversion.onehot_to_jgene(conversion.jgene_to_onehot(gene)) == gene
+
+@pytest.mark.skipif(CHAIN != "beta",
+                    reason="This test requires beta chain mode")
+def test_cdr3_length_of_onehots():
+    data = common.read_data_csv('adaptive-filter-test.correct.csv')
+    lengths = data['amino_acid'].apply(len).apply(float)
+    onehots = conversion.unpadded_tcrbs_to_onehot(data, 30)
+    assert lengths.equals(conversion.cdr3_length_of_onehots(onehots['amino_acid']))
+
+@pytest.mark.skipif(CHAIN != "delta",
+                    reason="This test requires delta chain mode")
+def test_cdr3_length_of_onehots_delta():
+    data = common.read_data_csv('delta_1000_test.csv')
+    lengths = data['amino_acid'].apply(len).apply(float)
+    onehots = conversion.unpadded_tcrbs_to_onehot(data, 30)
+    assert lengths.equals(conversion.cdr3_length_of_onehots(onehots['amino_acid']))
+
+@pytest.mark.skipif(CHAIN != "gamma",
+                    reason="This test requires gamma chain mode")
+def test_cdr3_length_of_onehots_gamma():
+    data = common.read_data_csv('gamma_1000_test.csv')
+    lengths = data['amino_acid'].apply(len).apply(float)
+    onehots = conversion.unpadded_tcrbs_to_onehot(data, 30)
+    assert lengths.equals(conversion.cdr3_length_of_onehots(onehots['amino_acid']))
+
+@pytest.mark.skipif(CHAIN != "beta",
+                reason="This test requires beta chain mode")   
+def test_contiguous_match_counts_df():
+    test = conversion.unpadded_tcrbs_to_onehot(common.read_data_csv('adaptive-filter-test.correct.csv'), 30)
+    v_germline_tensor, j_germline_tensor = conversion.adaptive_aa_encoding_tensors(30)
+    result = conversion.contiguous_match_counts_df(test, v_germline_tensor, j_germline_tensor)
+
+    assert np.array_equal(result[0], np.array([4., 6.]))
+    assert np.array_equal(result[1], np.array([5., 5.]))
+    assert np.array_equal(result[5], np.array([5., 0.]))
+
+@pytest.mark.skipif(CHAIN != "beta",
+                reason="This test requires beta chain mode")   
+def test_contiguous_match_counts_df():
+    test = conversion.unpadded_tcrbs_to_onehot(common.read_data_csv('adaptive-filter-test.correct.csv'), 30)
+    v_germline_tensor, j_germline_tensor = conversion.adaptive_aa_encoding_tensors(30)
+    result = conversion.contiguous_match_counts_df(test, v_germline_tensor, j_germline_tensor)
+
+    assert np.array_equal(result[0], np.array([4., 6.]))
+    assert np.array_equal(result[1], np.array([5., 5.]))
+    assert np.array_equal(result[5], np.array([5., 0.]))
+
+@pytest.mark.skipif(CHAIN != "delta",
+                reason="This test requires delta chain mode")   
+def test_contiguous_match_counts_df_delta():
+    test = conversion.unpadded_tcrbs_to_onehot(common.read_data_csv('delta_1000_test.csv'), 30)
+    v_germline_tensor, j_germline_tensor = conversion.adaptive_aa_encoding_tensors(30)
+    result = conversion.contiguous_match_counts_df(test, v_germline_tensor, j_germline_tensor)
+    
+    assert np.array_equal(result[0], np.array([4., 6.]))
+    assert np.array_equal(result[1], np.array([5., 5.]))
+    assert np.array_equal(result[5], np.array([5., 0.]))
 
 
 def test_pad_middle():
@@ -14,13 +115,6 @@ def test_pad_middle():
     assert '----' == conversion.pad_middle('---', 4)
 
 
-def test_gene_conversion():
-    for gene in conversion.TCRB_V_GENE_LIST:
-        assert conversion.onehot_to_vgene(conversion.vgene_to_onehot(gene)) == gene
-    for gene in conversion.TCRB_J_GENE_LIST:
-        assert conversion.onehot_to_jgene(conversion.jgene_to_onehot(gene)) == gene
-
-
 def test_aa_conversion():
     target = 'CASY'
     assert conversion.onehot_to_seq(conversion.seq_to_onehot(target)) == target
@@ -28,11 +122,6 @@ def test_aa_conversion():
     assert conversion.onehot_to_seq(conversion.seq_to_onehot(target)) == target
 
 
-def test_cdr3_length_of_onehots():
-    data = common.read_data_csv('adaptive-filter-test.correct.csv')
-    lengths = data['amino_acid'].apply(len).apply(float)
-    onehots = conversion.unpadded_tcrbs_to_onehot(data, 30)
-    assert lengths.equals(conversion.cdr3_length_of_onehots(onehots['amino_acid']))
 
 
 def test_contiguous_match_counts():
@@ -60,12 +149,3 @@ def test_contiguous_match_counts():
     assert np.array_equal(
         conversion.contiguous_match_counts(v01_j02_matching, v01_v02_mixture, j01_02), np.array([2.75, 6.]))
 
-
-def test_contiguous_match_counts_df():
-    test = conversion.unpadded_tcrbs_to_onehot(common.read_data_csv('adaptive-filter-test.correct.csv'), 30)
-    v_germline_tensor, j_germline_tensor = conversion.adaptive_aa_encoding_tensors(30)
-    result = conversion.contiguous_match_counts_df(test, v_germline_tensor, j_germline_tensor)
-
-    assert np.array_equal(result[0], np.array([4., 6.]))
-    assert np.array_equal(result[1], np.array([5., 5.]))
-    assert np.array_equal(result[5], np.array([5., 0.]))
